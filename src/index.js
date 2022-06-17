c253b9ecfd5bf80553e952f4a1f298e31c3a2184
OLESIA <alopex2012@gmail.com>
2022-06-17 23:12:33 +0300

    Added same functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+let apiKey = "c110ea55f9fc67bbe11e8618e106eff8";
 let now = new Date();
 function currentData(anyDate) {
   let hours = anyDate.getHours();
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
   let minutes = anyDate.getMinutes();
   if (minutes < 10) {
     minutes = "0" + minutes;
   }
+
   let day = anyDate.getDay();
   let days = [
     "Sunday",
@@ -23,12 +28,16 @@ function currentData(anyDate) {
 let setDate = document.querySelector(".currentTime");
 setDate.innerHTML = currentData(now);
 
+let tempC = null;
+
 let favCities = document.querySelectorAll(".favorites");
-let curCity = document.querySelector("span.currentCity");
+let currentCity = document.querySelector("span.currentCity");
+//let curCity = document.querySelector("span.currentCity");
 
 Array.from(favCities).forEach(function (city) {
   city.addEventListener("click", function (e) {
-    curCity.innerHTML = e.target.id;
+    //curCity.innerHTML = e.target.id;
+    search(e.target.id);
   });
 });
 
@@ -39,24 +48,27 @@ function setCity(event) {
   event.preventDefault();
 
   let cityName = document.querySelector("#search");
-  let currentCity = document.querySelector("span.currentCity");
 
   if (cityName.value.length > 0) {
-    function showData(response) {
-      if (response.status === 200) {
-        let city = response.data.name;
-        let temp = Math.round(response.data.main.temp);
-        let tempInС = document.querySelector("span.TempC");
-        tempInС.innerHTML = temp;
-        currentCity.innerHTML = city;
-      }
-    }
-    let apiKey = "c110ea55f9fc67bbe11e8618e106eff8";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=${apiKey}&units=metric`;
-    axios.get(url).then(showData);
+    search(cityName.value);
   }
 }
 searchCity.addEventListener("click", setCity);
+
+function search(cityName) {
+  function showData(response) {
+    if (response.status === 200) {
+      let city = response.data.name;
+      tempC = Math.round(response.data.main.temp);
+      let tempInС = document.querySelector("span.TempC");
+      tempInС.innerHTML = tempC;
+      currentCity.innerHTML = city;
+      startC();
+    }
+  }
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showData);
+}
 
 let currentPlace = document.querySelector(".current");
 function setPlace(event) {
@@ -67,16 +79,16 @@ function setPlace(event) {
   function showData(response) {
     if (response.status === 200) {
       let city = response.data.name;
-      let temp = Math.round(response.data.main.temp);
+      tempC = Math.round(response.data.main.temp);
       let tempInС = document.querySelector("span.TempC");
-      tempInС.innerHTML = temp;
+      tempInС.innerHTML = tempC;
       currentCity.innerHTML = city;
+      startC();
     }
   }
   function myPosition(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
-    let apiKey = "c110ea55f9fc67bbe11e8618e106eff8";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showData);
   }
@@ -89,24 +101,32 @@ let visible = document.querySelector(".currentTemp");
 let unvisible = document.querySelector(".hiddenTemp");
 function setTempF(event) {
   event.preventDefault();
+  if (unvisible.classList.contains("hiddenTemp") ) {
   unvisible.classList.add("currentTemp");
   visible.classList.remove("currentTemp");
   visible.classList.add("hiddenTemp");
   unvisible.classList.remove("hiddenTemp");
-  let tempInC = document.querySelector("span.TempC");
-  let tempC = parseInt(tempInC.innerText);
+  //let tempInC = document.querySelector("span.TempC");
+  //let tempC = parseInt(tempInC.innerText);
   tempInF.innerHTML = Math.round((tempC * 9) / 5 + 32);
+}
 }
 visible.addEventListener("click", setTempF);
 
 function setTempC(event) {
   event.preventDefault();
+  startC();
+}
+unvisible.addEventListener("click", setTempC);
+
+function startC() {
   unvisible.classList.remove("currentTemp");
   visible.classList.add("currentTemp");
   visible.classList.remove("hiddenTemp");
   unvisible.classList.add("hiddenTemp");
 }
-unvisible.addEventListener("click", setTempC);
+
+search("Amsterdam");
 
 /*
   alert(

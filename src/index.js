187ea3f4201de7a283ce1b85dca1982c8c2adff5
OLESIA <alopex2012@gmail.com>
2022-06-18 16:01:47 +0300

    Fixed bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ let tempC = null;
 let humValue = null;
 let windValue = null;
 let time = null;
-//let now = new Date();
+let setDate = document.querySelector(".currentTime");
+let favCities = document.querySelectorAll(".favorites");
+let currentCity = document.querySelector("p.currentCity");
+let visible = document.querySelector(".currentTemp");
+let unvisible = document.querySelector(".hiddenTemp");
+let tempInF = document.querySelector("span.TempF");
+let tempInС = document.querySelector("span.TempC");
+let searchCity = document.querySelector(".search");
+let humidity = document.querySelector("p.humidity");
+let wind = document.querySelector("p.wind");
+let description = document.querySelector("p.currentDescription");
+let currentPicture = document.querySelector(".currentPicture");
+
 function currentData(anyDate) {
   let realTime = new Date(anyDate);
   let hours = realTime.getHours();
@@ -27,17 +39,8 @@ function currentData(anyDate) {
   ];
   let fullDay = days[day];
   let result = `Valid for ${fullDay} ${hours}:${minutes} `;
-
   return result;
 }
-let setDate = document.querySelector(".currentTime");
-//setDate.innerHTML = currentData(time);
-
-
-
-let favCities = document.querySelectorAll(".favorites");
-
-let currentCity = document.querySelector("p.currentCity");
 
 Array.from(favCities).forEach(function (city) {
   city.addEventListener("click", function (e) {
@@ -45,51 +48,14 @@ Array.from(favCities).forEach(function (city) {
   });
 });
 
-let tempInF = document.querySelector("span.TempF");
-let tempInС = document.querySelector("span.TempC");
-let searchCity = document.querySelector(".search");
-let humidity = document.querySelector("p.humidity");
-let wind = document.querySelector("p.wind");
-let description = document.querySelector("p.currentDescription");
-let currentPicture = document.querySelector(".currentPicture");
-
 function setCity(event) {
   event.preventDefault();
-
   let cityName = document.querySelector("#search");
-
   if (cityName.value.length > 0) {
     search(cityName.value);
   }
 }
 searchCity.addEventListener("click", setCity);
-
-/* function showData(response) {
-    if (response.status === 200) {
-      let city = response.data.name;
-      time = response.data.dt * 1000;
-      setDate.innerHTML = currentData(time);
-
-      tempC = Math.round(response.data.main.temp);
-      tempInС.innerHTML = tempC;
-      currentCity.innerHTML = city;
-      console.log(response.data);
-      description.innerHTML = response.data.weather[0].description;
-      humValue = `Humidity ${response.data.main.humidity}%`;
-      humidity.innerHTML = humValue;
-      windValue = `Wind ${Math.round(response.data.wind.speed)} m/c`;
-      wind.innerHTML = windValue;
-      let icon = response.data.weather[0].icon;
-      console.log(icon);
-      currentPicture.setAttribute(
-        "src",
-        `http://openweathermap.org/img/w/${icon}.png`
-      );
-      startC();
-    }
-  } */
-  
-
 
 function search(cityName) {
   function showData(response) {
@@ -97,45 +63,58 @@ function search(cityName) {
       let city = response.data.name;
       time = response.data.dt * 1000;
       setDate.innerHTML = currentData(time);
-
       tempC = Math.round(response.data.main.temp);
       tempInС.innerHTML = tempC;
       currentCity.innerHTML = city;
-      console.log(response.data);
+      //console.log(response.data);
       description.innerHTML = response.data.weather[0].description;
       humValue = `Humidity ${response.data.main.humidity}%`;
       humidity.innerHTML = humValue;
       windValue = `Wind ${Math.round(response.data.wind.speed)} m/c`;
       wind.innerHTML = windValue;
       let icon = response.data.weather[0].icon;
-      console.log(icon);
+      let alt = response.data.weather[0].main;
       currentPicture.setAttribute(
         "src",
         `http://openweathermap.org/img/w/${icon}.png`
       );
+      currentPicture.setAttribute("alt", alt);
       startC();
     }
   }
-  //showData(response);
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showData);
+
+  let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+  axios.get(urlApi).then(showData);
 }
 
 let currentPlace = document.querySelector(".current");
 function setPlace(event) {
   event.preventDefault();
-
   function showData(response) {
     if (response.status === 200) {
       let city = response.data.name;
+      time = response.data.dt * 1000;
+      setDate.innerHTML = currentData(time);
       tempC = Math.round(response.data.main.temp);
-      let tempInС = document.querySelector("span.TempC");
       tempInС.innerHTML = tempC;
       currentCity.innerHTML = city;
+      //console.log(response.data);
+      description.innerHTML = response.data.weather[0].description;
+      humValue = `Humidity ${response.data.main.humidity}%`;
+      humidity.innerHTML = humValue;
+      windValue = `Wind ${Math.round(response.data.wind.speed)} m/c`;
+      wind.innerHTML = windValue;
+      let icon = response.data.weather[0].icon;
+      let alt = response.data.weather[0].main;
+      currentPicture.setAttribute(
+        "src",
+        `http://openweathermap.org/img/w/${icon}.png`
+      );
+      currentPicture.setAttribute("alt", alt);
       startC();
     }
   }
-  
+
   function myPosition(position) {
     let lat = position.coords.latitude;
     let lon = position.coords.longitude;
@@ -147,8 +126,6 @@ function setPlace(event) {
 }
 currentPlace.addEventListener("click", setPlace);
 
-let visible = document.querySelector(".currentTemp");
-let unvisible = document.querySelector(".hiddenTemp");
 function setTempF(event) {
   event.preventDefault();
   if (unvisible.classList.contains("hiddenTemp")) {
@@ -156,7 +133,6 @@ function setTempF(event) {
     visible.classList.remove("currentTemp");
     visible.classList.add("hiddenTemp");
     unvisible.classList.remove("hiddenTemp");
-
     tempInF.innerHTML = Math.round((tempC * 9) / 5 + 32);
     let humidityF = document.querySelector("p.humidityF");
     let windF = document.querySelector("p.windF");
@@ -179,10 +155,4 @@ function startC() {
   unvisible.classList.add("hiddenTemp");
 }
 
-window.onload = search("Kyiv");
-
-/*
-  alert(
-    `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${newCity}`
-  );
-   */
+search("Amsterdam");
